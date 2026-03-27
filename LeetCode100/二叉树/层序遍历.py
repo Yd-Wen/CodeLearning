f219ed05e74342cf591b8895d5270d1bea1a3372
Yindong Wen <wenyindong@163.com>
2026-03-27 10:36:25 +0800

    新增二叉树6-7

---
+++ LeetCode100/二叉树/层序遍历.py
@@ -0,0 +1,29 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+  def levelOrder(self, root):
+    """
+    :type root: Optional[TreeNode]
+    :rtype: List[List[int]]
+    """
+    res = []
+    def level(nodes):
+      if not nodes: return
+      r = []
+      new_nodes = []
+      for node in nodes:
+        if not node: continue
+        r.append(node.val)
+        if node.left:
+          new_nodes.append(node.left)
+        if node.right:
+          new_nodes.append(node.right)
+      if len(r): res.append(r)
+      level(new_nodes)
+    level([root])
+    return res  
+    
